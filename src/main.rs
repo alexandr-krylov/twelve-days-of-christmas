ab303bb3942f2f476b87547dcfa07e9e2ad20850
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-20 14:02:43 +0600

    orders of days

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    let order = ["first", "second", "third", "fourth", "fifth", "sixth",
+        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
 }
