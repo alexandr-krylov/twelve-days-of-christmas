080ed62605afa7725dc57cab72badf0609b25bd8
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-21 01:20:14 +0600

    in progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::ops::Index;
+
 fn main() {
-    println!("Hello, world!");
-    let order = ["first", "second", "third", "fourth", "fifth", "sixth",
-        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+    let order = ["first", "second",
+     "third", "fourth", "fifth",
+     "sixth", "seventh", "eighth",
+     "ninth", "tenth", "eleventh",
+     "twelfth"];
+    let gifts = ["A partridge in a pear tree", "Two turtle doves,",
+     "Three French hens", "Four calling birds", "Five golden rings",
+     "Six geese a-laying", "Seven swans a-swimming", "Eight maids a-milking",
+     "Nine ladies dancing", "Ten lords a-leaping", "Eleven pipers piping",
+     "Twelve drummers drumming"];
+    for number_love in order.iter() {
+        println!("{},{:#?}", number_love, order.iter);
+    }
 }
